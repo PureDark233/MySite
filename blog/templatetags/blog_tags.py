5a02be9e70a4892cd9578141cfb17bc78d8bc1c6
puredark233 <zfan1998@foxmail.com>
2019-03-20 11:32:06 +0800

    Adding tags and pagination support.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
-from ..models import Post,Category
+from ..models import Post,Category, Tag
 from django import template
+from django.db.models.aggregates import Count
 
 register =template.Library()
 
@@ -14,3 +15,7 @@ def get_recent_posts(num=5):
 @register.simple_tag
 def get_categories():
     return Category.objects.all()
+
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,147 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category, Tag
 from comments.forms import CommentForm
 import markdown
+from django.views.generic import ListView, DetailView
+
+
 # Create your views here.
 
-def index(request):
-    post_list=Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list':post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 1
 
-def archives(request, year, month):
-    post_list=Post.objects.filter(created_time__year=year,
-                                  created_time__month=month).order_by('created_time')
-    return render(request, 'blog/index.html', context={'post_list':post_list})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
 
-def detail(request, pk):
-    post= get_object_or_404(Post, pk=pk)
-    post.increase_views()
-    post.body=markdown.markdown(post.body,
-                                extensions=[
-                                    'markdown.extensions.extra',
-                                    'markdown.extensions.codehilite',
-                                    'markdown.extensions.toc',
-                                ])
-    form = CommentForm()
-    comment_list=post.comment_set.all()
-    context={
-        'post':post,
-        'form':form,
-        'comment_list':comment_list,
-    }
-    return render(request, 'blog/detail.html', context=context)
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+        return data
+
+    def get_queryset(self):
+        return super(IndexView, self).get_queryset().order_by('-created_time')
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
+                                                               created_time__month=month).order_by('created_time')
+class TagView(IndexView):
+    def get_queryset(self):
+        tag=get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body, extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
+# def index(request):
+#     post_list=Post.objects.all().order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list':post_list})
+# def archives(request, year, month):
+#     post_list = Post.objects.filter(created_time__year=year,
+#                                     created_time__month=month).order_by('created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     post.increase_views()
+#     post.body = markdown.markdown(post.body,
+#                                   extensions=[
+#                                       'markdown.extensions.extra',
+#                                       'markdown.extensions.codehilite',
+#                                       'markdown.extensions.toc',
+#                                   ])
+#     form = CommentForm()
+#     comment_list = post.comment_set.all()
+#     context = {
+#         'post': post,
+#         'form': form,
+#         'comment_list': comment_list,
+#     }
+#     return render(request, 'blog/detail.html', context=context)
