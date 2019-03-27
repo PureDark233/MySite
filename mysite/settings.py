d18f2c711d17c6dd7d181a8c9f887cedd535fc72
puredark233 <zfan1998@foxmail.com>
2019-03-27 15:33:41 +0800

    Configs for deployment.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^dpyq^@gm3vc8=1j+bz4l*9qi)&6x9iuq*txrmnx_+61)7)vn5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.puredark233.site']
 
 
 # Application definition
@@ -133,3 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
